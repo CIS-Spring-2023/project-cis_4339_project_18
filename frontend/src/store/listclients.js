6c9ed39430738751f4e832c57c28666036243505
ItzelMM <85587774+ItzelMM@users.noreply.github.com>
2023-03-13 17:11:53 -0500

    Adding comments and cleaning files
    Deleted some files that were never used and added comments for clients store

--- frontend/src/store/listclients.js
+++ frontend/src/store/listclients.js
@@ -1,5 +1,6 @@
 //The general structure of this code is from: https://hackernoon.com/build-a-single-page-web3-app-with-vue-3-vite-and-pinia 
 
+// At first I tried to featch the data for the pie graph using a pinia store, I couldn't make it work but I didn't delete the store file because I think it can maybe be used when we implement the backend
 import { defineStore } from 'pinia'
 
 export const useClientsStore = defineStore('clients',{
