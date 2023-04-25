3ee557aeb503701d8a5887eeb2b865d906ebb433
altran28 <89654443+altran28@users.noreply.github.com>
2023-04-25 09:10:46 -0500

    Update login.js
    Added my version

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -31,3 +31,37 @@ router.post('/login', function(req, res) {
     }
   })
 })
+
+
+
+
+// Anthony code:
+
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+// importing data model schemas
+const { login } = require('../models/models')
+
+// GET 10 most recent events for org
+router.get('/', (req, res, next) => {
+    const username = req.body
+    const password = req.body
+    const role = req.body
+
+    if (!username || !password) {
+        return res.status(400).json({ error: "Username and password are required" });
+    }
+        
+    try {
+        const login = await loginDataSchema.findOne({ username });
+        if (!login || !(await bcrypt.compare(password, login.password))) {
+        return res.status(400).json({ error: "Incorrect login" });
+        }
+        
+        res.json(login);
+        } catch (error) {
+        console.error(error);
+        return next(error);
+        }
+
