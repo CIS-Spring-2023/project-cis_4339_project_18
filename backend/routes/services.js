a3a7471e88eae6f2fce36fb4e30483b481f23819
mamart64 <89712486+mamart64@users.noreply.github.com>
2023-04-24 19:45:23 -0500

    Add files via upload
    services api

---
+++ backend/routes/services.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const router = express.Router()
+
+const org = process.env.ORG
+
+// importing data model schemas
+const { services } = require('../models/models')
+
+// GET 10 most recent services for org
+router.get('/', (req, res, next) => {
+  services
+    .find({ org: org }, (error, data) => {
+      if (error) {
+        return next(error)
+      } else {
+        return res.json(data)
+      }
+    })
+    // sort by date ascending
+    .sort({ date: 1 })
+    .limit(10)
+})
+
+// POST new service
+router.post('/', (req, res, next) => {
+  const newService = req.body
+  newService.org = org
+  services.create(newService, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+// PUT update service
+router.put('/update/:id', (req, res, next) => {
+  services.findByIdAndUpdate(req.params.id, req.body, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+// hard DELETE service by ID, as per project specifications
+router.delete('/:id', (req, res, next) => {
+  services.findByIdAndDelete(req.params.id, (error, data) => {
+    if (error) {
+      return next(error)
+    } else if (!data) {
+      res.status(400).send('Service not found')
+    } else {
+      res.send('Service deleted')
+    }
+  })
+})
+
+module.exports = router
